6c84e5edaa9c7cfe766da044e2ab57d3cd059f03
danilo <danilo.o.santos@hotmail.com>
2025-05-22 22:58:20 -0300

    conectando o server ao fornt

---
+++ server/main.cjs
@@ -0,0 +1,18 @@
+﻿const express = require('express')
+const app = express()
+
+app.get('/api/test', (req, res) => {
+  res.json({ 
+    status: 'OK',
+    message: 'API recriada com sucesso!',
+    timestamp: new Date().toISOString()
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`
+   Servidor rodando na porta ${PORT}
+   Teste em: http://localhost:${PORT}/api/test
+  `)
+})
